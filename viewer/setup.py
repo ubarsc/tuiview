264e5ff164b2df52b1f850682b6ee0d0fb1e9ea4
Pete Bunting <petebunting@mac.com>
2012-08-20 20:52:19 +0100

    An edit to python setup.py to support py2app mac equivalent to py2exe. The py2app part does not currently work but it doesn't have broken anything else.

--- viewer/setup.py
+++ viewer/setup.py
@@ -22,7 +22,7 @@ kwargs = {'name':'viewer',
       'author_email':'gillingham.sam@gmail.com',
       'scripts':['bin/viewer'],
       'packages':['viewer'],
-      'license':'LICENSE.txt', 
+      'license':'LICENSE.txt',
       'url':'https://bitbucket.org/chchrsc/viewer'}
 
 # are we building in executeable under Windows?
@@ -38,22 +38,46 @@ if py2exe:
         shutil.rmtree(distdir)
     except:
         pass
-    
+
     # bundle the runtime
     data_files = [("Microsoft.VC90.CRT", glob(r'C:\Program Files\Microsoft Visual Studio 9.0\VC\redist\x86\Microsoft.VC90.CRT\*.*'))]
     kwargs['data_files'] = data_files
     kwargs['windows'] = ['bin/viewer']
     # see http://www.py2exe.org/index.cgi/TkInter, http://www.py2exe.org/index.cgi/Py2exeAndPyQt
     # gone all out on the optimisations - don't need docstrings etc
-    options = {'py2exe':{'bundle_files' : 3, 'includes':["sip"], 
+    options = {'py2exe':{'bundle_files' : 3, 'includes':["sip"],
                 'excludes':["pywin", "pywin.debugger", "pywin.debugger.dbgcon", "pywin.dialogs", "pywin.dialogs.list",
                 "Tkconstants","Tkinter","tcl"], 'optimize':2, 'dist_dir' : distdir}}
     kwargs['options'] = options
-    
+
+# are we building in executeable under Mac OSX?
+py2appcmd = len(sys.argv) > 1 and sys.platform == 'darwin' and sys.argv[1] == 'py2app'
+
+if py2appcmd:
+    from setuptools import setup
+    import shutil
+    distdir = 'dist'
+    try:
+        shutil.rmtree(distdir)
+    except:
+        pass
+
+    app = ['viewer/viewerapplication.py']
+    #app = ['bin/viewer']
+    data_files = []
+    options = {'py2app':{'argv_emulation': True, 'dist_dir': distdir, 'includes': ['sip']}}
+
+    kwargs['data_files'] = data_files
+    kwargs['app'] = app
+    kwargs['options'] = options
+    kwargs['setup_requires'] = 'py2app'
+
+
+
 # now run setup with the options we have collected
+print kwargs
 setup(**kwargs)
 
 if py2exe:
     # don't need this file
     os.remove(os.path.join(distdir, 'w9xpopen.exe'))
-    
