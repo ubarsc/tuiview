fe4f50e3cc2e1fe98e8cef50b1a2a868d0b940b0
Daniel Clewley <daniel.clewley@gmail.com>
2014-11-26 09:54:33 +0000

    Added license text for archive reader.

--- tuiview/archivereader.py
+++ tuiview/archivereader.py
@@ -1,3 +1,31 @@
+"""
+Check if file within a zip archive and return apropriate GDAL path if so.
+
+See GDAL documentation on reading files within a zip archive.
+
+http://trac.osgeo.org/gdal/wiki/UserDocs/ReadInZip
+
+Currently only checks for files with the extension .bil
+"""
+# This file is part of 'TuiView' - a simple Raster viewer
+# Copyright (C) 2012  Sam Gillingham
+#
+# archivereader.py written by Terry Cain (2014), Plymouth Marine Laboratory 
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 import zipfile
 
 def file_list_to_archive_strings(filenames):
