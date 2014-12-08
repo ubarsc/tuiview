9d910a7b6e404af81f9bf969a341e36220780d45
Terry Cain <terry@dolphincorp.co.uk>
2014-12-08 15:18:35 +0000

    Opens more files inside of ZIP archives
    tuiview/archivereader.py
      When looking for files inside of a ZIP archive the function now checks for a range of file extensions in the tuple 'ALLOWED_FILE_EXTENSIONS'
      Added sphinx documentation to the functions

--- tuiview/archivereader.py
+++ tuiview/archivereader.py
@@ -4,8 +4,6 @@ Check if file within a zip archive and return apropriate GDAL path if so.
 See GDAL documentation on reading files within a zip archive.
 
 http://trac.osgeo.org/gdal/wiki/UserDocs/ReadInZip
-
-Currently only checks for files with the extension .bil
 """
 # This file is part of 'TuiView' - a simple Raster viewer
 # Copyright (C) 2012  Sam Gillingham
@@ -28,8 +26,18 @@ Currently only checks for files with the extension .bil
 
 import zipfile
 
-def file_list_to_archive_strings(filenames):
+ALLOWED_FILE_EXTENSIONS = ('.bil', '.bsq', '.bin', '.env', '.dem', '.tif', '.asc', '.img')
 
+def file_list_to_archive_strings(filenames):
+    """
+    Modifies any archive filenames in the list to allow them to be opened using a GDAL virtual filesystem.
+
+    :param filenames: A list of filenames
+    :type filenames: list of str
+
+    :return: The list of filenames passed in, with any supported archive prepended with the appropriate string.
+    :rtype: list of str
+    """
     output_filename_list = []
 
     for filename in filenames:
@@ -42,22 +50,34 @@ def file_list_to_archive_strings(filenames):
 
     return output_filename_list
 
-def gz_to_file(filename):
+def gz_to_file(filepath):
     """
-    Converts a .gz filepath so that it can be opened with GDAL
-    """
-    return "/vsigzip/" + filename
+    Converts a path ending in .gz so that it can be opened with GDAL
 
-def zip_to_file(filename):
+    :param filepath: Filepath ending in .gz
+    :type filepath: str
+
+    :return: Filepath prepended with gzip virtual filesystem string
+    :rtype: str
     """
-    Takes in a zip filename and if the zip contains BIL files then the filename will be converted
-    so that it can be opened without extraction,
+    return "/vsigzip/" + filepath
+
+def zip_to_file(filepath):
+    """
+    Takes in a zip filepath and if the zip contains files that can be opened with GDAL then the filepath will be converted
+    so that it can be opened without extraction.
+
+    :param filepath: Filepath ending in .zip
+    :type filepath: str
+
+    :return: A list of files prepended with the zip virtual filesystem string
+    :rtype: list of str
     """
     zip_file_list = []
-    if zipfile.is_zipfile(filename):
+    if zipfile.is_zipfile(filepath):
         try:
-            zip_file = zipfile.ZipFile(filename)
-            zip_file_contents = ['/vsizip/{0}/{1}'.format(filename, zip_info_object.filename) for zip_info_object in zip_file.filelist if zip_info_object.filename.endswith('.bil')]
+            zip_file = zipfile.ZipFile(filepath)
+            zip_file_contents = ['/vsizip/{0}/{1}'.format(filepath, zip_info_object.filename) for zip_info_object in zip_file.filelist if zip_info_object.filename.endswith(ALLOWED_FILE_EXTENSIONS)]
             zip_file_list.extend(zip_file_contents)
             zip_file.close()
         except zipfile.BadZipfile:
