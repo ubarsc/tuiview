d66ce31178dee7babbd9e7c740d5299c8086eeed
Sam Gillingham <gillingham.sam@gmail.com>
2016-01-04 09:45:59 +1000

    updated to version 1.1.7

--- tuiview/__init__.py
+++ tuiview/__init__.py
@@ -3,5 +3,5 @@ TuiView - this file is needed to ensure it can be imported
 
 See other source files for details
 """
-TUIVIEW_VERSION = '1.1.6'
+TUIVIEW_VERSION = '1.1.7'
 __version__ = TUIVIEW_VERSION
