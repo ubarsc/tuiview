b4a2837acf033193195c02343341d183910207ef
Sam Gillingham <gillingham.sam@gmail.com>
2019-02-18 13:51:14 +1000

    update to version 1.2.4

--- tuiview/__init__.py
+++ tuiview/__init__.py
@@ -3,5 +3,5 @@ TuiView - this file is needed to ensure it can be imported
 
 See other source files for details
 """
-TUIVIEW_VERSION = '1.2.3'
+TUIVIEW_VERSION = '1.2.4'
 __version__ = TUIVIEW_VERSION
