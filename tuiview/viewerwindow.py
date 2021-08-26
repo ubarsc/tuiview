3199c5c749dbc402c666bcafa9669d9cac18c331
Sam Gillingham <gillingham.sam@gmail.com>
2021-08-27 09:04:39 +1000

    catch other potential crashes

--- tuiview/viewerwindow.py
+++ tuiview/viewerwindow.py
@@ -1209,9 +1209,10 @@ File will now be opened using default stretch""")
         Query dock window has been closed. Disconnect from
         vectorLocationSelected signal and decrement our count
         """
-        self.viewwidget.vectorLocationSelected.disconnect(
+        if self.vectorQueryWindowCount > 0:
+            self.viewwidget.vectorLocationSelected.disconnect(
                         queryDock.vectorLocationSelected)
-        self.vectorQueryWindowCount -= 1
+            self.vectorQueryWindowCount -= 1
 
     def profile(self, checked):
         """
@@ -1260,8 +1261,9 @@ File will now be opened using default stretch""")
         Profile dock window has been closed. Disconnect from
         polylineCollected signal and decrement our count
         """
-        self.viewwidget.polylineCollected.disconnect(profileDock.newLine)
-        self.profileWindowCount -= 1
+        if self.profileWindowCount > 0:
+            self.viewwidget.polylineCollected.disconnect(profileDock.newLine)
+            self.profileWindowCount -= 1
 
     def properties(self):
         """
