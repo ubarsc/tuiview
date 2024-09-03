40c83c8de3f4ab265474d8c3f9637020b7a5289c
Sam Gillingham <gillingham.sam@gmail.com>
2024-09-03 14:07:16 +1000

    keep flake8 happy

--- tuiview/profilewindow.py
+++ tuiview/profilewindow.py
@@ -29,7 +29,6 @@ import numpy
 
 from . import plotwidget
 from .plotscalingdialog import PlotScalingDialog
-from .viewertoolclasses import PolylineToolInfo
 
 
 class ProfileDockWidget(QDockWidget):
